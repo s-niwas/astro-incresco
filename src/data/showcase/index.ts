4376acb80d3b54f4c73b55cbb73c45d9d2086a32
Varun GM <111367533+varungm24@users.noreply.github.com>
2023-07-14 15:04:40 +0530

    feat(client-contact):- add storyblok integration for client and contact section (#5)
    Co-authored-by: GM Varun <gmvarun@GMs-MacBook-Air.local>

--- src/data/showcase/index.ts
+++ src/data/showcase/index.ts
@@ -1,29 +1,32 @@
 // adapted from https://github.com/withastro/astro.build/blob/112bdc723b3ba305997c95d7ce02304624d0d3ce/src/data/showcase/index.ts
 
 import type { ShowcaseSite } from "~/types";
-import sitesData from "./sites.json";
+import { useStoryblokApi } from "@storyblok/astro";
 
-const allImages = import.meta.glob<ImageMetadata>("./images/*.{png,jpg,jpeg}", {
-  eager: true,
-  import: "default",
-});
+const storyblokApi = useStoryblokApi();
+let content: { clients: any[] };
+try {
+  const { data } = await storyblokApi.get("cdn/stories/clientssection", {
+    version: import.meta.env.DEV ? "draft" : "published",
+  });
+  content = data.story.content;
+} catch (error) {}
 
 let _loadShowcase: Promise<Array<ShowcaseSite>>;
 
 async function loadShowcase(): Promise<Array<ShowcaseSite>> {
   const sites = await Promise.all(
-    sitesData.map(async (site) => {
-      if (!(site.image in allImages)) {
+    content?.clients?.map(async (site: any) => {
+      if (!site?.logo?.filename.startsWith("https")) {
         console.error(
-          `Image for "${site.title}" not found (provided: "${site.image}")`
+          `Image URL for "${site.title}" is invalid (provided: "${site?.logo?.filename}")`
         );
       }
-
-      const image = await allImages[site.image];
-
       return {
         ...site,
-        image,
+        image: {
+          src: site?.logo?.filename,
+        },
       };
     })
   );
